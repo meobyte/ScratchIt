791d1d788bad558ab4de0200c1df24390995ee6f
Michelle Anderson <meobyte@gmail.com>
2015-06-05 13:59:13 -0700

    Initial prototypish code. Test currently failing.

--- src/scratchit.js
+++ src/scratchit.js
@@ -5,11 +5,99 @@
  * Copyright (c) 2015 Michelle Anderson
  * Licensed under the MIT license.
  */
-(function ($) {
-  $.fn.ScratchIt = function () {
-    return this.each(function (i) {
-      // Do something to each selected element.
-      $(this).html('ScratchIt' + i);
+; (function ($, window, document, undefined) {
+  var x, y;
+  var defaults = {
+    propertyName: "value"
+  };
+
+  /**
+  * ScratchIt
+  * @constructor
+  * @param {Object} element
+  * @param {Object} options (Optional)
+  */
+    function ScratchIt(element, options) {
+        this.$element = $(element);
+
+        this.options = $.extend({}, defaults, options);
+
+        this._defaults = defaults;
+        this._name = 'scratchIt';
+
+        this.isScratching === undefined && this.init();
+
+    }
+
+    ScratchIt.prototype = {
+
+        init: function () {
+          
+            this.canvas = this.$element.find('canvas');
+            this.context = this.canvas[0].getContext('2d');
+            
+            var context = this.context; 
+             
+            this.topImage = this.$element.find('img').first();
+            this.bottomImage = this.topImage.next();
+            this.topImage = this.topImage[0];
+            
+            this.newImage = new Image();
+            this.newImage.src = this.topImage.src;
+            var newImage = this.newImage;
+            newImage.onload = function(){
+              context.drawImage(newImage, 0, 0);
+              context.globalCompositeOperation = "destination-out";
+            };
+            
+            console.log(this.newImage.src);
+            
+
+            this.context.strokeStyle = "#F00";
+            this.context.lineJoin = "round";
+            this.context.lineWidth = 20;
+            
+            this.offsetxy  = this.canvas.offset();
+            
+            this.canvas.on({
+              'mousedown.scratchit' : $.proxy(this.onDown, this),
+              'mousemove.scratchit' : $.proxy(this.onMove, this),
+              'mouseup.scratchit' : $.proxy(this.onUp, this)
+            });
+        },
+        
+        onDown: function(e) {
+          
+          var context = this.context;
+          context.beginPath();
+          x = e.pageX;
+          y = e.pageY - this.offsetxy.top;
+          context.moveTo(x, y);
+          this.isScratching = true;
+
+        },
+        onMove: function(e) {
+          var context = this.context;
+          if (!this.isScratching) {return;}
+          x = e.pageX;
+          y = e.pageY - this.offsetxy.top;
+          context.lineTo(x, y);
+          context.stroke();
+
+        },
+        onUp: function () {
+          this.isScratching = false;
+
+        }
+    };
+
+  $.fn.scratchIt = function (options) {
+    return this.each(function () {
+      if (!$.data(this, 'scratchIt')) {
+        $.data(this, 'ScratchIt',
+                    new ScratchIt(this, options));
+      }
+
     });
   };
-}(jQuery));
+})(jQuery, window, document);
--- test/scratchit_test.js
+++ test/scratchit_test.js
@@ -1,30 +1,58 @@
 (function ($) {
   module('jQuery#ScratchIt', {
     setup: function () {
-      var canvas, context;
       this.elems = $('#qunit-fixture').children();
-      canvas = document.getElementById('qunit-canvas');
-      context = canvas.getContext('2d');
-      this.canvas = canvas;
-      this.context = context;
+      this.elem = $('#qunit-fixture');
     }
   });
 
   test('is chainable', function () {
     expect(1);
-    strictEqual(this.elems.ScratchIt(), this.elems, 'should be chainable');
+    strictEqual(this.elems.scratchIt(), this.elems, 'should be chainable');
   });
 
   test('is ScratchIt', function () {
     expect(1);
-    strictEqual(this.elems.ScratchIt().text(), 'ScratchIt0ScratchIt1ScratchIt2', 'should be ScratchIt');
+    strictEqual(this.elems.scratchIt().text(), 'ScratchIt0ScratchIt1ScratchIt2', 'should be ScratchIt');
+  });
+  test('init', function(assert){
+    assert.ok(this.elem.scratchIt().init);
+  });
+  /* 
+  test('init Canvas', function(assert){
+    this.elem.scratchIt().init.context.fillStyle = 'rgba(0, 0, 0, 0)';
+    this.elem.scratchIt().init.context.fillRect(0, 0, 5, 5);
+
+    assert.pixelEqual(this.elem.scratchIt().init.canvas, 0, 0, 0, 0, 0, 0);
+    assert.notPixelEqual(this.elem.scratchIt().init.canvas, 0, 0, 1, 1, 1, 0);
   });
 
-  test('has Canvas', function(assert) {
+ module('Canvas', {
+    setup: function () {
+      var canvas, context;
+      canvas = $('#qunit-fixture').find('canvas')[0];
+
+      try {
+        context = canvas.getContext('2d');
+      }
+      catch (e) {
+        // probably no canvas support, just exit
+        return;
+      }
+
+      this.canvas = canvas;
+      this.context = context;
+    }
+  });
+
+  test('Canvas pixels from manual fills', function (assert) {
+    expect(2);
     this.context.fillStyle = 'rgba(0, 0, 0, 0)';
     this.context.fillRect(0, 0, 5, 5);
-  
+
     assert.pixelEqual(this.canvas, 0, 0, 0, 0, 0, 0);
     assert.notPixelEqual(this.canvas, 0, 0, 1, 1, 1, 0);
   });
-}(jQuery));
+*/
+
+} (jQuery));
